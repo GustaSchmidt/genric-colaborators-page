730929dc563038035091cf31781e5091f3dd4f70
Gustavo Schmidt <gustavooliveira092011@gmail.com>
2021-08-04 13:29:07 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function loadJSONData(source_){
     xobj.send(null);
     return JSON.parse(xobj.responseText);
 }
-function do_cards(id, foto, nome, cargo){
+function mount_card_string(id, foto, nome, cargo){
     var card_string = `
     <div class="col-3 card">
         <div class="card-warper">
@@ -22,8 +22,13 @@ function do_cards(id, foto, nome, cargo){
         </div>
     </div>
     `;
-    console.log(card_string);
+    return card_string;
 }
 var data = loadJSONData('./Dados/dados.json');
+var cards;
+for (let index = 0; index < data.length; index++) {
+    console.log(index);
+    //TODO: append de cards no warper de cards
+}
 console.log(data[0]);
 do_cards(data[0].id, data[0].foto, data[0].nome, data[0].cargo, data[0].idade);
